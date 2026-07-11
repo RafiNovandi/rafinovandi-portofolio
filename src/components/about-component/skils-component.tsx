e69a8fab9a00df36a2b7a486ece1787892f18091
Muhammad Rafi Novandi <mrafinovandi04@gmail.com>
2026-07-11 12:19:13 +0700

    update: layout skills

--- src/components/about-component/skils-component.tsx
+++ src/components/about-component/skils-component.tsx
@@ -1,48 +1,60 @@
 export default function ShowSkills() {
   return (
-    <div className="h-auto w-225 mt-20">
+    <div className="h-fit w-300 mt-20 bg-white p-10 rounded-2xl">
       <h1 className="text-3xl font-bold">Skills</h1>
       <h3>My professional skills.</h3>
-      <div className="flex flex-wrap gap-3 py-5">
-        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-4xl">
-          <div className="w-5 h-5 rounded-sm bg-black"></div>
-          <div>NextJS</div>
-        </div>
-        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-4xl">
-          <div className="w-5 h-5 rounded-sm bg-black"></div>
-          <div>Typecript</div>
-        </div>
-        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-4xl">
-          <div className="w-5 h-5 rounded-sm bg-black"></div>
-          <div>Supabase</div>
-        </div>
-        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-4xl">
-          <div className="w-5 h-5 rounded-sm bg-black"></div>
-          <div>HTML</div>
-        </div>
-        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-4xl">
-          <div className="w-5 h-5 rounded-sm bg-black"></div>
-          <div>CSS</div>
-        </div>
-        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-4xl">
-          <div className="w-5 h-5 rounded-sm bg-black"></div>
-          <div>SASS</div>
-        </div>
-        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-4xl">
+      <div className="flex flex-wrap gap-2.5 py-5">
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
           <div className="w-5 h-5 rounded-sm bg-black"></div>
           <div>Javascript</div>
         </div>
-        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-4xl">
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
           <div className="w-5 h-5 rounded-sm bg-black"></div>
-          <div>SASS</div>
+          <div>Typecript</div>
         </div>
-        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-4xl">
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
+          <div className="w-5 h-5 rounded-sm bg-black"></div>
+          <div>Next.js</div>
+        </div>
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
           <div className="w-5 h-5 rounded-sm bg-black"></div>
           <div>Supabase</div>
         </div>
-        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-4xl">
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
           <div className="w-5 h-5 rounded-sm bg-black"></div>
-          <div>Typecript</div>
+          <div>Shadcn IU</div>
+        </div>
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
+          <div className="w-5 h-5 rounded-sm bg-black"></div>
+          <div>HTML</div>
+        </div>
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
+          <div className="w-5 h-5 rounded-sm bg-black"></div>
+          <div>CSS</div>
+        </div>
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
+          <div className="w-5 h-5 rounded-sm bg-black"></div>
+          <div>SASS</div>
+        </div>
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
+          <div className="w-5 h-5 rounded-sm bg-black"></div>
+          <div>Javascript</div>
+        </div>
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
+          <div className="w-5 h-5 rounded-sm bg-black"></div>
+          <div>SASS</div>
+        </div>
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
+          <div className="w-5 h-5 rounded-sm bg-black"></div>
+          <div>Supabase</div>
+        </div>
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
+          <div className="w-5 h-5 rounded-sm bg-black"></div>
+          <div>Angular</div>
+        </div>
+        <div className="flex items-center justify-center bg-gray-300 gap-2 px-4 py-3 rounded-[15px]">
+          <div className="w-5 h-5 rounded-sm bg-black"></div>
+          <div>Tailwind CSS</div>
         </div>
       </div>
     </div>
