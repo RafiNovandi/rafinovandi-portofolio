8020c866a24ad0e4ebf2f6bb2fec03d0666ac9aa
Muhammad Rafi Novandi <mrafinovandi04@gmail.com>
2026-07-14 23:54:43 +0700

    feat(about): make skill content have adaptive background and dynamic text color

--- src/app/constants/skill.ts
+++ src/app/constants/skill.ts
@@ -9,25 +9,30 @@ export const skill = [
     title: "Next.js",
     icon: nextjsIcon,
     bgColor: "#FFFFFF",
+    textColor: "#000000",
   },
   {
     title: "Github",
     icon: githubIcon,
     bgColor: "#FFFFFF",
+    textColor: "#000000",
   },
   {
     title: "Gitlab",
     icon: gitlabIcon,
     bgColor: "#FFCB9D",
+    textColor: "#741A0C",
   },
   {
     title: "Tailwind",
     icon: tailwindIcon,
     bgColor: "#D8F2FD",
+    textColor: "#0A4965",
   },
   {
-    title: "TypeScrip",
+    title: "Typescript",
     icon: typescriptIcon,
     bgColor: "#BFEBFF",
+    textColor: "#0C2948",
   },
 ];
--- src/components/about-component/skils-component.tsx
+++ src/components/about-component/skils-component.tsx
@@ -1,17 +1,18 @@
 import { skill } from "@/app/constants/skill";
+import { color } from "motion";
 import Image from "next/image";
 
 export default function ShowSkills() {
   return (
-    <div className="h-fit w-full max-w-4xl mx-5 md:mx-0 mt-20 bg-white p-5 md:p-10 rounded-2xl">
+    <div className="h-fit w-full max-w-4xl mx-5 md:mx-0 mt-20 bg-white dark:bg-gray-400 p-5 md:p-10 rounded-2xl">
       <h1 className="text-3xl font-bold">Skills</h1>
       <h3>My professional skills.</h3>
       <div className="flex flex-wrap gap-2.5 py-5">
         {skill.map((item, index) => (
           <div
             key={index}
-            className="flex items-center justify-center gap-2 px-4 py-2.5 rounded-3xl border-1"
-            style={{ backgroundColor: item.bgColor}}
+            className="flex items-center justify-center gap-2 px-4 py-2.5 rounded-3xl border"
+            style={{ backgroundColor: item.bgColor }}
           >
             <Image
               src={item.icon}
@@ -20,7 +21,7 @@ export default function ShowSkills() {
               height={20}
               className="rounded-sm object-contain"
             />
-            <div>{item.title}</div>
+            <div style={{ color: item.textColor }}>{item.title}</div>
           </div>
         ))}
       </div>
