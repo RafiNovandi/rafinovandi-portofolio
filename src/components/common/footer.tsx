5adb331ab506cb0648b18f13c812d7e21b5028b4
Muhammad Rafi Novandi <mrafinovandi04@gmail.com>
2026-07-14 23:40:16 +0700

    fix: footer layout

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="text-sm flex flex-col md:flex-row items-center justify-between p-5 w-full px-5 md:px-20 border-t border-solid border-gray-200 bg-accen text-gray-400 gap-5 md:gap-10 text-center md:text-left">
+    <div className="text-sm mx-auto max-w-screen-7xl px-6 py-4.5 flex items-center justify-between gap-4 border-solid border-gray-200 bg-accen text-gray-400 md:gap-10 text-center md:text-left">
       <div>
         © 2026 Portfolio · Built with Next.js, Tailwind CSS, Framer Motion &
         shadcn/ui
